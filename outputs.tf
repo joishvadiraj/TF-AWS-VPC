52d53d8bb3bdcfa1cbaa52623bb41e726b257253
joishvadiraj <joish.vadiraj@gmail.com>
2024-12-05 22:08:25 +0100

    reorganizing

--- outputs.tf
+++ outputs.tf
@@ -4,14 +4,12 @@ output "vpc_id" {
     description = "VPC id of my-aws-vpc "
   
 }
-
 # Public Subnet
 output "my-pub-subnet-id" {
     value = aws_subnet.my-pub-subnet.id
     description = "Subnet ID of my-pub-subnet "
   
 }
-
 # Private Subnet
 output "my-pri-subnet-id" {
     value = aws_subnet.my-pri-subnet.id
